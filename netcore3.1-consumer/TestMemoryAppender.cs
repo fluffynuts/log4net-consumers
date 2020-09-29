0205bf20b4e714d3185f352056028619bf4c7672
Davyd McColl <davydm@gmail.com>
2020-09-29 08:47:20 +0200

    :white_check_mark: add more assertions to prove log comes out

--- netcore3.1-consumer/TestMemoryAppender.cs
+++ netcore3.1-consumer/TestMemoryAppender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using log4net;
 using log4net.Appender;
@@ -17,27 +18,38 @@ namespace log4net_consumer
         [Test]
         public void ShouldLog()
         {
+            // Arrange
             var repo = LogManager.GetRepository(typeof(TestMemoryAppender).Assembly);
             var appender = new MemoryAppender();
             BasicConfigurator.Configure(repo, appender);
 
             var logger = LogManager.GetLogger(typeof(TestMemoryAppender));
-            logger.Logger.Log(new LoggingEvent(
-                new LoggingEventData()
-                {
-                    TimeStampUtc = DateTime.UtcNow,
-                    LoggerName = "Logger",
-                    ThreadName = Thread.CurrentThread.ManagedThreadId.ToString(),
-                    Level = Level.Info,
-                    Message = "Hello",
-                    Properties = new PropertiesDictionary()
-                })
-            );
+            var data = new LoggingEventData()
+            {
+                TimeStampUtc = DateTime.UtcNow,
+                LoggerName = "Logger",
+                ThreadName = Thread.CurrentThread.ManagedThreadId.ToString(),
+                Level = Level.Info,
+                Message = "Hello",
+                Properties = new PropertiesDictionary()
+            };
+            var ev = new LoggingEvent(data);
             
+            // Act
+            logger.Logger.Log(ev);
+
+            // Assert
             var events = appender.GetEvents();
-            
+
             Expect(events)
                 .To.Contain.Only(1).Item();
+            var captured = events.Single().GetLoggingEventData();
+            Expect(captured.Level)
+                .To.Equal(Level.Info);
+            Expect(captured.Message)
+                .To.Equal(data.Message);
+            Expect(captured.LoggerName)
+                .To.Equal(data.LoggerName);
         }
     }
 }
